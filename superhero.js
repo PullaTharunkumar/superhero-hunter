6825679d810e13ef52217cada35b66cc020f6cbe
PullaTharunkumar <tharunkumar20004@gmail.com>
2023-10-25 15:58:20 +0530

    url changes

--- superhero.js
+++ superhero.js
@@ -31,8 +31,8 @@ const render = async (data) => {
     detailsHtml = ''
     // Looping Superheroe data (comics, events, series, stories)
     for (let i of dataList) {
-        const url = i.resourceURI + `?ts=0&apikey=2f4a049d7e302a9993962a89b95b28fe&hash=64ef51991e88c83111f824e3b6106a13` // url for fetchig data
-        const res = await fetch(url)
+        // const url = i.resourceURI + `?ts=0&apikey=2f4a049d7e302a9993962a89b95b28fe&hash=64ef51991e88c83111f824e3b6106a13` // url for fetchig data
+        const res = await fetch(`${i.resourceURI}?ts=0&apikey=2f4a049d7e302a9993962a89b95b28fe&hash=64ef51991e88c83111f824e3b6106a13`)
         const jsonRes = await res.json(); // (comics, events, series, stories) character json data from marvel api
         const item = jsonRes?.data?.results[0]
         const itemImg = item?.thumbnail ? item?.thumbnail?.path + '.' + item?.thumbnail?.extension : 'https://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg'
@@ -43,21 +43,21 @@ const render = async (data) => {
 
 // comics Event hander
 const comics = async () => {
-    render('comics') // Displaying comics
+    await render('comics') // Displaying comics
 }
 
 // events Event hander
-const events = () => {
-    render('events') // Displaying comics events
+const events = async () => {
+    await render('events') // Displaying comics events
 }
 
 //series Event hander
-const series = () => {
-    render('series') // Displaying comics series
+const series = async () => {
+    await render('series') // Displaying comics series
 }
 // stories Event hander
-const stories = () => {
-    render('stories') // Displaying comics stories
+const stories = async () => {
+    await render('stories') // Displaying comics stories
 }
 
 init()
